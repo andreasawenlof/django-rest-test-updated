8318ea5218b2521ee4ff97cf808c579dced5cc8c
mezter1337 <andreas.javert@gmail.com>
2025-01-19 13:16:34 +0100

    added profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 """
 This file contains the views for the profiles app.
 """
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Profile
 from .serializers import ProfileSerializer
+from .models import Profile
 
 
 class ProfileList(APIView):
@@ -16,3 +18,33 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Retrieve a profile
+    """
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        """
+        Get a profile by its primary key.
+        """
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
