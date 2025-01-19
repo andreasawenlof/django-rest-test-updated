666efdeb4961b42476899b821babc1611a2ffffa
mezter1337 <andreas.javert@gmail.com>
2025-01-19 21:45:58 +0100

    Added filter backends and filtersets to posts and profiles

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ This file contains the views for the profiles app.
 """
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .serializers import ProfileSerializer
 from .models import Profile
@@ -18,9 +19,15 @@ class ProfileList(generics.ListAPIView):
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('created_at')
+
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
     ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+    ]
+
     ordering_fields = [
         'posts_count',
         'followers_count',
