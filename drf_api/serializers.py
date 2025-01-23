a72a9b068d85a4c2612ee38b682b72e8c4e7d312
mezter1337 <andreas.javert@gmail.com>
2025-01-23 16:28:34 +0100

    Fixing serializer

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -6,6 +6,11 @@ class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        print("CurrentUserSerializer called! Data:", data)  # Debugging line
+        return data
+
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
             'profile_id',
