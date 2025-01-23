a430971815d1eba928658cc113a2ea4315318529
mezter1337 <andreas.javert@gmail.com>
2025-01-23 07:02:55 +0100

    fix that auth class was no tuple

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -29,9 +29,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-    )],
+    ],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
